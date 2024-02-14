e8369f393979883200f1bb5a02724fc1e7a60e83
ludmila-omlopes <ludmila.omlopes@gmail.com>
2024-02-13 22:37:49 -0300

    logs

--- src/pages/api/generate-ens-image.ts
+++ src/pages/api/generate-ens-image.ts
@@ -109,6 +109,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   // Convert the address to lowercase
   address = address.toLowerCase();
+  console.log('Determined Address:', address);
+  console.log('Trusted Data:', trustedData.address);
 
   try {
     const { data } = await client.query({
